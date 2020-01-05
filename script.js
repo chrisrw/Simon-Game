40e166c4ca4f6bbc9d83af5f508aa054648de562
chrisrw <crwargo0783@gmail.com>
2020-01-05 15:14:07 -0500

    added start button and basic setup for score count via HTML/CSS

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// target squares
+
+
+// what does game do?
+    // 1) squares light up in a random pattern starting with just one light during the lightup sequence
+    // 2) user clicks, if sequence is clicked in the correct order, score is incremented by one
+    // 3) increment light sequence by one
+    // 4) if lightup sequence is clicked in the incorrect order, user loses and gains no points.
+    
+    // target squares
