5de4e0c1f2ddaf051b1029e3d1e5debf5a3d58a0
chrisrw <crwargo0783@gmail.com>
2020-01-05 17:37:46 -0500

    added some selectors and pseudocode

--- script.js
+++ script.js
@@ -6,4 +6,18 @@
     // 3) increment light sequence by one
     // 4) if lightup sequence is clicked in the incorrect order, user loses and gains no points.
     
+// press start button to start
+// show new sequence
+// user choose
+    // correct? if yes, is it the end of the sequence? if yes, add one to sequence if no, use choose. if no, game over 
+                
     // target squares
+    const greenSquare = document.querySelector('.green');
+    const yellowSquare = document.querySelector('.yellow');
+    const redSquare = document.querySelector('red');
+    const blueSquare = document.querySelector('blue'); 
+
+    greenSquare.addEventListener('click', test)
+    function test(test){
+        console.dir(test)
+    }
